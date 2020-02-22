fab9391c16cb29e5b6a38aecbd4a3a3a8aa4c401
Sofian Ardiansyah Wahab <limadawai@gmail.com>
2020-02-22 15:01:39 +0700

    Update terbaru lagi

--- src/siswa/FormInput.java
+++ src/siswa/FormInput.java
@@ -2,8 +2,16 @@ package siswa;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -30,52 +38,52 @@ public class FormInput extends JFrame {
         
         //********* Main Panel **********
         mainPanel = new JPanel();
-        mainPanel.setPreferredSize(new Dimension(400, 750));
+        mainPanel.setPreferredSize(new Dimension(750, 950));
         mainPanel.setBackground(Color.white);
         
         //************ Panel NIM ****************
         JPanel panelNim = new JPanel(new BorderLayout());
-        panelNim.setPreferredSize(new Dimension(390, 25));
+        panelNim.setPreferredSize(new Dimension(740, 25));
         panelNim.setBackground(Color.white);
         
-        JLabel labelNim = new JLabel("NIM               :");
+        JLabel labelNim = new JLabel("NIM                    :");
         JTextField textNim = new JTextField(30);
         panelNim.add(labelNim, BorderLayout.WEST);
         panelNim.add(textNim);
         
         //************ Panel Nama ****************
         JPanel panelNama = new JPanel(new BorderLayout());
-        panelNama.setPreferredSize(new Dimension(390, 25));
+        panelNama.setPreferredSize(new Dimension(740, 25));
         panelNama.setBackground(Color.white);
         
-        JLabel labelNama = new JLabel("Nama            :");
+        JLabel labelNama = new JLabel("Nama                 :");
         JTextField textNama = new JTextField(25);
         panelNama.add(labelNama, BorderLayout.WEST);
         panelNama.add(textNama);
         
         //************ Panel Prodi ****************
         JPanel panelProdi = new JPanel(new BorderLayout());
-        panelProdi.setPreferredSize(new Dimension(390, 25));
+        panelProdi.setPreferredSize(new Dimension(740, 25));
         panelProdi.setBackground(Color.white);
         
-        JLabel labelProdi = new JLabel("Program Studi   :");
+        JLabel labelProdi = new JLabel("Program Studi    :");
         JTextField textProdi = new JTextField(25);
         panelProdi.add(labelProdi, BorderLayout.WEST);
         panelProdi.add(textProdi);
         
         //************ Panel Alamat ****************
         JPanel panelAlamat = new JPanel(new BorderLayout());
-        panelAlamat.setPreferredSize(new Dimension(390, 25));
+        panelAlamat.setPreferredSize(new Dimension(740, 25));
         panelAlamat.setBackground(Color.white);
         
-        JLabel labelAlamat = new JLabel("Alamat        :");
+        JLabel labelAlamat = new JLabel("Alamat               :");
         JTextField textAlamat = new JTextField(25);
         panelAlamat.add(labelAlamat, BorderLayout.WEST);
         panelAlamat.add(textAlamat);
         
         //************ Panel Buttons ****************
-        JPanel panelButton = new JPanel(new BorderLayout());
-        panelButton.setPreferredSize(new Dimension(390, 25));
+        JPanel panelButton = new JPanel(new FlowLayout());
+        panelButton.setPreferredSize(new Dimension(740, 30));
         panelButton.setBackground(Color.white);
         
         JButton btnAdd = new JButton("Add");
@@ -87,12 +95,10 @@ public class FormInput extends JFrame {
         panelButton.add(btnDelete);
         
         //************ Panel JTable ****************
-        JPanel panelTab = new JPanel(null);
-        String[] column = {"NIM","Nama","Alamat","Prodi"};
-        DefaultTableModel dfm = new DefaultTableModel(null, column);
-        JTable table = new JTable(dfm);
-        JScrollPane scrollPane = new JScrollPane(table);
-        panelTab.add(scrollPane);
+        JPanel panelTab = new JPanel(new BorderLayout());
+        panelTab.setPreferredSize(new Dimension(740, 100));
+        
+        panelTab.add(mytab(), BorderLayout.CENTER);
         
         mainPanel.add(panelNim);
         mainPanel.add(panelNama);
@@ -103,6 +109,51 @@ public class FormInput extends JFrame {
         
         add(mainPanel, BorderLayout.CENTER);
         setVisible(true);
+        
+        btnAdd.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String nim = textNim.getText();
+				String nama = textNama.getText();
+				String prodi = textProdi.getText();
+				String alamat = textAlamat.getText();
+				Koneksi koneksi = new Koneksi();
+				koneksi.insert(nim, nama, prodi, alamat);
+			}
+		});
 	}
+	
+	
+	
+	public JScrollPane mytab() {
+		DefaultTableModel dfm = new DefaultTableModel();
+        dfm.addColumn("NIM");
+        dfm.addColumn("Nama");
+        dfm.addColumn("Alamat");
+        dfm.addColumn("Prodi");
+        try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/bnsp?autoreconnect=true&useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false","root", "root");
+			String sql = "SELECT * FROM siswa";
+	        PreparedStatement st = (PreparedStatement) connection.prepareStatement(sql);
+	        ResultSet rs = st.executeQuery();
+	        while (rs.next()) {
+	        	String nim = rs.getString("nim");
+	        	String nama = rs.getString("nama");
+	        	String prodi = rs.getString("prog_studi");
+	        	String alamat = rs.getString("alamat");
+	        	dfm.addRow(new Object[] {nim, nama, prodi, alamat});
+	        }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+        JTable table = new JTable(dfm);
+        table.setBounds(100, 100, 100, 80);
+        JScrollPane scrollPane = new JScrollPane(table);
+		return scrollPane;
+	}
+	
 
 }
--- src/siswa/Koneksi.java
+++ src/siswa/Koneksi.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Koneksi {
 	
@@ -29,6 +31,51 @@ public class Koneksi {
 			e.printStackTrace();
 		}
 		return false;
+	}
+	
+	public void insert(String nim, String nama, String prodi, String alamat) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/bnsp?autoreconnect=true&useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false","root", "root");
+			String sql = "INSERT INTO siswa(nim, nama, prog_studi, alamat) VALUES(?, ?, ?, ?)";
+	        PreparedStatement st = (PreparedStatement) connection.prepareStatement(sql);
+	        st.setString(1, nim);
+	        st.setString(2, nama);
+	        st.setString(3, prodi);
+	        st.setString(4, alamat);
+	        ResultSet rs = st.executeQuery();
+	        connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<String> record() {
+		List result = new ArrayList<String>();
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/bnsp?autoreconnect=true&useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false","root", "root");
+			String sql = "SELECT * FROM siswa";
+	        PreparedStatement st = (PreparedStatement) connection.prepareStatement(sql);
+	        ResultSet rs = st.executeQuery();
+	        while (rs.next()) {
+	        	String nim = rs.getString("nim");
+	        	String nama = rs.getString("nama");
+	        	String prodi = rs.getString("prog_studi");
+	        	String alamat = rs.getString("alamat");
+	        	result.add(nim);
+	        	result.add(nama);
+	        	result.add(prodi);
+	        	result.add(alamat);
+	        }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return (ArrayList<String>) result;
 	}
 
 }
